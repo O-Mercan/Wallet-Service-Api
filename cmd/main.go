5a78b663376a44e7dc6e62df7a338d78b4a135db
Ömer Mercan <70708913+O-Mercan@users.noreply.github.com>
2022-08-08 13:18:35 +0300

    added go mud config

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
+// App - the struct with contains like pointers
+// database connections
+type App struct{}
+
+func (a *App) Run() error {
+	fmt.Println()
+}
+
 func main() {
 
 }
